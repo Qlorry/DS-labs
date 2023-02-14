9846391c2e26b3dda97193cd3748aac8bbaa3c7f
qlorry <kostik.cu4er@gmail.com>
2023-02-14 15:18:00 +0200

    busines logic for facade and logging srv

--- facade-service/FacadeApp.py
+++ facade-service/FacadeApp.py
@@ -1,10 +1,13 @@
 from http.server import HTTPServer
 import argparse
+from functools import partial
 
 from FacadeImpl import *
 from FacadeApp import *
 
 CONFIG_KEY_PORT = "PORT"
+CONFIG_KEY_LOG_SERVICE = "LOG_ADDR"
+CONFIG_KEY_MSG_SERVICE = "MSG_ADDR"
 
 class FacadeApp:
     def __init__(self):
@@ -21,15 +24,20 @@ class FacadeApp:
 
     def parse_args(self):
         args = self.arg_parser.parse_args()
-        if args.port != None:
-            self.config[CONFIG_KEY_PORT] = args.port
+        self.config[CONFIG_KEY_PORT] = args.port
+        self.config[CONFIG_KEY_LOG_SERVICE] = args.log_service
+        self.config[CONFIG_KEY_MSG_SERVICE] = args.message_service
 
     def add_args_to_parser(self):
         self.arg_parser.add_argument('-p', '--port', type=int) 
+        self.arg_parser.add_argument('-ls', '--log-service', type=str) 
+        self.arg_parser.add_argument('-ms', '--message-service', type=str) 
+
 
     def run(self):
         server_address = ('', self.config[CONFIG_KEY_PORT])
-        httpd = HTTPServer(server_address, FacadeImpl)
+        handler = partial(FacadeImpl, self.config[CONFIG_KEY_LOG_SERVICE], self.config[CONFIG_KEY_MSG_SERVICE])
+        httpd = HTTPServer(server_address, handler)
         app_log('Starting httpd at ' + str(server_address))
         try:
             httpd.serve_forever()
--- logging-service/LoggingImpl.py
+++ logging-service/LoggingImpl.py
@@ -2,22 +2,29 @@ from http.server import BaseHTTPRequestHandler
 
 from Logging import *
 
+stored_messages = dict()
+
 class LoggingImpl(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+    def _set_response(self, code):
+        self.send_response(code)
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
 
     def do_GET(self):
-        domain_log("GET request,\nPath: %s\nHeaders:\n%s".format( str(self.path), str(self.headers)))
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        domain_log("GET request,\nPath: {0}\nHeaders:\n{1}".format( str(self.path), str(self.headers)))
+        res = ""
+        for value in stored_messages:
+            res += stored_messages[value] + "\n"
+        self._set_response(200)
+        self.wfile.write(res.encode('utf-8'))
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        domain_log("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s".format(
+        domain_log("POST request,\nPath: {0}\nHeaders:\n{1}\n\nBody:\n{2}".format(
                 str(self.path), str(self.headers), post_data.decode('utf-8')))
 
-        self._set_response()
+        stored_messages[self.headers["ID"]] = post_data.decode('utf-8')
+
+        self._set_response(200)
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
