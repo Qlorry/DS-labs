b3724570b8e8b40c17a7311f72de3f7669cad4d0
qlorry <kostik.cu4er@gmail.com>
2023-02-14 10:51:55 +0200

    basic server from google

---
+++ facade-service/main.py
@@ -0,0 +1,24 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+from FacadeImpl import *
+
+def run(server_class=HTTPServer, handler_class=FacadeImpl, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
